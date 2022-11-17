ba16e08b84b503b75682bb8c3616453578602a3f
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-11-17 17:02:03 +0300

    On barcode detection

--- src/screens/casher.js
+++ src/screens/casher.js
@@ -1,4 +1,4 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import Header from '../components/header';
 import Report from '../components/Report/report';
 import './casher.css';
@@ -6,9 +6,35 @@ import './casher.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Ticketing from '../components/ticketing';
 
+import ScannerModal from '../components/scanner_modal';
+import useScanDetection from 'use-scan-detection';
+
 function Casher() {
+    const [barcode, setBarcode] = useState(null)
+    const [barcodeModalShow, setBarcodeModalShow] = useState(false)
+    const [billNumber, setBillNumber] = useState(null)
+    
+
+    function closeModal(){
+        setBarcodeModalShow(false)
+    }
+    function onBarcodeDetected(code){
+        if(code !== null){
+            setBarcode(code)
+            setBarcodeModalShow(true)
+        }
+    }
+    
+    useScanDetection({
+        onComplete: (code) => {
+            onBarcodeDetected(code)
+        },
+        minLength:3
+    })
+
     return (
         <Fragment>
+        { barcodeModalShow && (<ScannerModal closeModal={closeModal} billNumber={barcode}/>)}
             <Header/>
             <Routes>
                 <Route index element={<Ticketing/>}/>
